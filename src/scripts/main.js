4c0a7f58e04d9cdf7cc388ab62a647398220dd9d
Deman250 <deman.250052@gmail.com>
2019-11-25 07:17:13 +0200

    final solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+$(document).ready(function() {
+  $('#select').styler();
+
+  $('.grats__galery-box').lightGallery({
+    thumbnail: true,
+    // animateThumb: false,
+    // showThumbByDefault: false,
+  });
+});
